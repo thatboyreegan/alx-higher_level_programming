6beb55e09864b46709c3fc075256f888a1a40c3d
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2023-03-13 12:01:26 +0300

    Fourth correction

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,4 +3,4 @@
 
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
-        print("{:d}".format(mylist[i]))
+        print("{:d}".format(my_list[i]))
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(my_list[i]))
