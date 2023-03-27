2c62cb04859a8701431e5ba69ac22493d98d3b0b
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2023-03-27 21:52:56 +0300

    Second corrction

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,11 +6,10 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end=" ")
-        except ValueError:
-            pass
-        except TypeError:
-            pass
-        else:
             real += 1
+        except ValueError:
+            continue
+        except TypeError:
+            continue
     print("")
     return real
