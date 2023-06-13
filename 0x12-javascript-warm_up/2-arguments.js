c384ebb316f86f82bfe8163c96f2e046535ba1d4
Reagan Ronald Odhiambo <reaganronald491@gmail.com>
2023-06-13 03:23:47 +0300

    made changes to the output of 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,6 +4,8 @@ import('node:process');
 
 if (!process.argv[2]) {
   console.log('No argument');
-} else {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
